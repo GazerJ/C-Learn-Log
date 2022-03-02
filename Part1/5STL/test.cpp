f4f91a79c6790d2d98c3b980e4a5f7bcc9b2df33
gazer <gazerj@foxmail.com>
2022-03-02 08:11:59 +0800

    STL

---
+++ Part1/5STL/test.cpp
@@ -0,0 +1,54 @@
+/*
+Maker : GazerJ
+Date: 2021.12.30
+What: *** app in Sever/Client Computer
+How: ***
+Way: Init=>*=>*=>*=>*=>*=>*=>Close
+*/
+#include <iostream>
+#include <vector>
+#include <array>
+#include <deque> 
+using namespace std;
+int main(){
+	//随机访问迭代器 ：可以指定下标   array,vector,deque   (array 理解为顺序表，需要指定大小如array<int,10>)
+	//单项访问迭代器： 自动单链表指针，不能指定下标，可以比较指针顺序 list ,map ,set
+	// 双向迭代其器：  循环链表指针，不能指定下标，不可以比较指针顺序 forward_list,unordered_map ,unordered_set
+	//主要的由begin()和end()方法 
+	 //迭代器：随机访问迭代器 ： 
+	 
+	 
+	vector<int> values; 
+	values.push_back(1);
+	values.push_back(2);
+	for(int i =0 ;i<values.size();i++) cout<<values[i]<<endl;
+	vector<int>::iterator i;//迭代器/者的本质是包装过后的指针   
+	for (i=values.begin();i<values.end();i++) cout<<*i<<endl;
+	for (i=values.begin();i!=values.end();i++) cout<<*i<<endl;
+	array<int ,10> v{1,1,2,3,4,5,5,6,6,6};
+	for(int i =0 ;i<v.size();i++) cout<<v[i]<<endl;
+	int j=0;
+	for(int i= 0;i<v.size();i++){
+		if( v[i]==v[i+1]){
+			for(j=i;j<v.size();j++){
+				if (v[i]!=v[j]){
+					for(int k=i;k<j;k++){
+						v[k]='\0';
+					}
+					break;
+				}
+				if( j==v.size()-1){
+					for(int k=i;k<j+1;k++){
+						v[k]='\0';
+					}
+					break;
+				}
+			}
+		}
+	}
+	
+	for(int i =0 ;i<v.size();i++) cout<<v[i]<<endl;
+	
+	
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# This is learn-log for C++ 
+
+## Part 1
++ the  Basic Grammar of C++.
+	+ File
+	+ Class
+	+ Thread
+	+ ect  
+
+## Part 2
++ the Typical project
+	+ ...
+
