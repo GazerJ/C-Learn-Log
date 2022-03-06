3132bdbad174fde5fed59c0349b370f958237bb2
GazerJ <73857720+GazerJ@users.noreply.github.com>
2022-03-06 09:44:20 +0800

    commit

---
+++ Part2/1np/np.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
+void load(string filename,int len){
+    ifstream infile("filename");
+	string file;
+	infile >> file;
+	cout<<"read file \t"<<file<<endl;
+    infile >> file;
+	cout<<"read file \t"<<file<<endl;
+}
+
+
+
+int main(){
+    load("read.txt",2);
+	return 0;
+}
